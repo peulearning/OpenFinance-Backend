dc74e29c43ed7cf9d5d04acefc359067bb055a0c
Pedro <peuhenry2018@gmail.com>
2025-12-26 20:15:05 -0300

    Implementando Transaction ✨

--- backend/src/main/java/com/financeapp/backend/model/Transaction.java
+++ backend/src/main/java/com/financeapp/backend/model/Transaction.java
@@ -1,11 +1,15 @@
 package com.financeapp.backend.model;
 
 import java.math.BigDecimal;
-
+import java.time.LocalDate;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -22,7 +26,69 @@ public class Transaction {
 
   private BigDecimal amount; // Dinheiro
 
-  
+  @Enumerated(EnumType.STRING)
+  private TransactionType type; // Receita ou Despesa
 
+  private LocalDate date;
+
+
+  @ManyToOne
+  @JoinColumn(name = "user_id", nullable = false)
+  private User user;
+
+
+  public Transaction(){}
+
+
+  // Getter & Setter
+
+  public Long getId(){
+    return id;
+  }
+
+  public void setId(Long id){
+    this.id = id;
+  }
+
+  public String getDesription(){
+    return description;
+  }
+
+  public void setDescription(String description){
+    this.description = description;
+  }
+
+  public BigDecimal getAmount(){
+    return amount;
+  }
+
+  public void setAmount(BigDecimal amount){
+    this.amount = amount;
+  }
+
+  public TransactionType getType(){
+    return type;
+  }
+
+  public void setType(TransactionType type){
+    this.type = type;
+  }
+
+  public LocalDate getDate(){
+    return date;
+  }
+
+  public void setDate(LocalDate date){
+    this.date = date;
+  }
+
+  public User getUser(){
+    return user;
+  }
+
+  public void setUser(User user){
+    this.user = user;
+  }
+  
 
 }
