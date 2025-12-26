484e25658cf54f568ec4e5d77dcff06f9f5e0763
Pedro <peuhenry2018@gmail.com>
2025-12-25 21:38:41 -0300

    Add Swagger ✨

--- backend/src/main/java/com/financeapp/backend/dto/RegisterRequest.java
+++ backend/src/main/java/com/financeapp/backend/dto/RegisterRequest.java
@@ -5,29 +5,50 @@ import jakarta.validation.constraints.Size;
 
 public class RegisterRequest {
 
-  @NotBlank
-  private String name;
+    @NotBlank
+    private String name;
 
-  @NotBlank
-  private String email;
+    @NotBlank
+    private String email;
 
-  @NotBlank
-  @Size(min = 6, message = "Password must be at least 6 characters long")
-  private String password;
+    @NotBlank
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    private String password;
 
-  public String getPassword() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
-  }
+    // Construtor vazio (Obrigatório)
+    public RegisterRequest() {
+    }
 
-  public String getName() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getName'");
-  }
+    // Construtor completo
+    public RegisterRequest(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
-  public String getEmail() {
-    // TODO Auto-generated method stub
-    throw new UnsupportedOperationException("Unimplemented method 'getEmail'");
-  }
+    // Getters
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
-
