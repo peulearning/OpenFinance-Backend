8bd64842e8811dcf175b5971ca9d6ea1814e11c9
Pedro <peuhenry2018@gmail.com>
2025-12-26 21:38:56 -0300

    Implementando Repository

---
+++ backend/src/main/java/com/financeapp/backend/repository/TransactionRepository.java
@@ -0,0 +1,14 @@
+package com.financeapp.backend.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.financeapp.backend.model.Transaction;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+
+  //Buscar Transação pelo Email da acc
+  List<Transaction> findByAccountEmail(String email);
+}
